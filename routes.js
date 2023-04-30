f9c3c3f45aa1375e8de01dc8314a8aaca31648aa
guifrancisco <guifrancisco4@gmail.com>
2023-04-30 02:28:49 -0300

    feat: :sparkles: adding user home page

--- routes.js
+++ routes.js
@@ -5,10 +5,5 @@ const contatoController = require('./src/controllers/contatoController');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
-
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
-
 
 module.exports = route;
--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
+//app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-const sessionOptions = segitssion({
+const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
